75aefe8f46a7b4d212ffaeb0ae650a275af12598
saikumar <saikumarlaveti5061@gmail.com>
2025-03-24 22:58:02 +0530

    Greeting app

---
+++ Project_2_WishMessageApp/src/main/java/com/mvc/Project2WishMessageAppApplication.java
@@ -0,0 +1,13 @@
+package com.mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project2WishMessageAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project2WishMessageAppApplication.class, args);
+	}
+
+}
