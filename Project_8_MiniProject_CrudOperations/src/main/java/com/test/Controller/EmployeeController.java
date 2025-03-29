4f1428b1c6f432fd5f3e70f0b81e6617d901a5ed
saikumar <saikumarlaveti5061@gmail.com>
2025-03-30 00:55:14 +0530

    mini project start

---
+++ Project_8_MiniProject_CrudOperations/src/main/java/com/test/Controller/EmployeeController.java
@@ -0,0 +1,14 @@
+package com.test.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class EmployeeController {
+	
+	@GetMapping("/")
+	public String welcomePage() {
+		return "home";
+	}
+
+}
